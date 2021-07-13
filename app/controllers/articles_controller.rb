10370eb1d8b6e177d9f416639698836425377d47
fajarsj <fajarthea5@gmail.com>
2021-07-14 02:43:26 +0700

    add create and update features for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,41 @@
 class ArticlesController < ApplicationController
 
-    def show
-        # @ mean convert it from this block of code variable to an instance that can be use in view
-        @article = Article.find(params[:id])
-    end
+  def show
+    # @ mean convert it from this block of code variable to an instance that can be use in view
+    @article = Article.find(params[:id])
+  end
 
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      # @article_path(@article) is basically redirect you to @article.id, and you can shortened to @article
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 
 end
