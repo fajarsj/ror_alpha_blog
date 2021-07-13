0d1381c34326df0a9a730240bc896e39a56fc7f4
fajarsj <fajarthea5@gmail.com>
2021-07-13 11:41:40 +0700

    add articles listing feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController < ApplicationController
-  def show
-    # @ mean convert it from this block of code variable to an instance that can be use in view
-    @article = Article.find(params[:id])
-  end
+
+    def show
+        # @ mean convert it from this block of code variable to an instance that can be use in view
+        @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
 end
