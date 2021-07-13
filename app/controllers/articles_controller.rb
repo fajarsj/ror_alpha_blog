4e98925689d884c40feb86f321da5c597611fda7
fajarsj <fajarthea5@gmail.com>
2021-07-13 11:26:33 +0700

    add show articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,6 @@
+class ArticlesController < ApplicationController
+  def show
+    # @ mean convert it from this block of code variable to an instance that can be use in view
+    @article = Article.find(params[:id])
+  end
+end
