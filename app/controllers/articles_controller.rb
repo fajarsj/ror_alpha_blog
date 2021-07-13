b04c36e10567ab8efda218402c7929739d0a8b29
fajarsj <fajarthea5@gmail.com>
2021-07-14 04:16:54 +0700

    add hyperlink and refactor code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    # @ mean convert it from this block of code variable to an instance that can be use in view
-    @article = Article.find(params[:id])
   end
 
   def index
+    # @ mean convert it from this block of code variable to an instance that can be use in view
     @articles = Article.all
   end
 
@@ -14,11 +14,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
   
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully"
       # @article_path(@article) is basically redirect you to @article.id, and you can shortened to @article
@@ -29,8 +28,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -38,4 +36,21 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    # the "articles_" is get from Prefix when you run "rails routes --expanded"
+    redirect_to articles_path
+  end
+
+  # private method need to placed in the bottom of a controller, otherwise it will not work or certain method
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+#   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
