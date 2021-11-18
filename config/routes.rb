fef83b01e4b30d9c00e8bd6d1a981c855e330c0e
fajarsj <fajarthea5@gmail.com>
2021-11-18 19:47:42 +0700

    add new users form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 #   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
